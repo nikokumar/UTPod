e609099aa7fbe7af6843267770184906fe9e456b
nikokumar <nikolai.kumar@gmail.com>
2020-04-03 00:34:35 -0500

    Add files via upload

--- UtPod/UtPod.cpp
+++ UtPod/UtPod.cpp
@@ -2,8 +2,13 @@
 // Created by nikol on 4/1/2020.
 //
 #include <iostream>
+#include "Song.h"
 #include "UtPod.h"
 
 using namespace std;
 
+UtPod::int addSong(Song const &s) {
 
+    if(memSize - s.size < 0)
+
+}
