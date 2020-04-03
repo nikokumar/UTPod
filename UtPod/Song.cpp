56b804d2fe10a597740c678061dbff5adfde375b
nikokumar <nikolai.kumar@gmail.com>
2020-04-03 15:13:23 -0500

    Song Header and most of Song

--- UtPod/Song.cpp
+++ UtPod/Song.cpp
@@ -1,1 +1,43 @@
+//
+#include <string>
+class Song{
+private:
+    string title; //not const or static?
+    string artist;
+    int size;
 
+public:
+    Song(string title, string artist, int size);
+    //constructor sets variables
+    void setTitle(string t){
+        title = t;
+    }
+    string getTitle(){
+        return title;
+    }
+    void setArtist(string a){
+        artist = a;
+    }
+    string getArtist(){
+        return artist;
+    }
+    void setSize(int s){
+        size = s;
+    }
+    int getSize(){
+        return size;
+    }
+    bool operator == (Song const &s1, Song const &s2){
+        if(s1.getArtist()==s2.getArtist() &&
+           s1.getTitle()==s2.getTitle() &&
+           s1.getSize()==s2.getSize()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool operator < (Song const &s1, Song const &s2);
+    bool operator > (Song const &s1, Song const &s2);
+
+};
--- UtPod/Song.h
+++ UtPod/Song.h
@@ -2,11 +2,21 @@
 #include <string>
 class Song{
 private:
-    static const string title;
-    static const string artist;
-    static const int size;
+    string title; //not const or static?
+    string artist;
+    int size;
 
 public:
     Song(string title, string artist, int size);
     //constructor sets variables
+    void setTitle(string t);
+    string getTitle();
+    void setArtist(string a);
+    string getArtist();
+    void setSize(int s);
+    int getSize();
+    bool operator == (Song const &s1, Song const &s1);
+    bool operator < (Song const &s1, Song const &s1);
+    bool operator > (Song const &s1, Song const &s1);
+
 };
